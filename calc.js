39a9e75662884bf3b2539f6a8a75a8ffaf6bdbd1
RLaVilla <codingrlav@gmail.com>
2024-11-08 19:23:02 -0500

    completed basic calculator layout, made operator functions, and function to complete the operation by presing '='.

---
+++ calc.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+function operate(firstNumber, secondNumber, operator) {
+    if (operator === '+') {
+       return add(firstNumber, secondNumber);
+    }
+
+    if (operator === '-') {
+        return subtract(firstNumber, secondNumber);
+    }
+
+    if (operator === 'x') {
+        return multiply(firstNumber, secondNumber);
+    }
+
+    if (operator === '/') {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
